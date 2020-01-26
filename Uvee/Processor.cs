7c4e97b1492f4cc0cd3414bc6fce9a03cd69ebfb
Crauzer <filip.quitko@gmail.com>
2020-01-26 15:23:40 +0100

    1.0

---
+++ Uvee/Processor.cs
@@ -0,0 +1,57 @@
+﻿using Fantome.Libraries.League.IO.SimpleSkin;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.Primitives;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Uvee
+{
+    public static class Processor
+    {
+        private static readonly SolidBrush BRUSH = Brushes.Solid(Color.White);
+
+        public static void Process(string fileLocation, SKNFile skn)
+        {
+            string exportDirectory = string.Format(@"{0}\Uvee_{1}", Path.GetDirectoryName(fileLocation), Path.GetFileNameWithoutExtension(fileLocation));
+            Directory.CreateDirectory(exportDirectory);
+
+            foreach (SKNSubmesh submesh in skn.Submeshes)
+            {
+                Image image = CreateImage();
+                List<ushort> indices = submesh.GetNormalizedIndices();
+
+                //Loop through all submesh faces and draw the lines for them using vertex UV
+                for (int i = 0; i < indices.Count;)
+                {
+                    SKNVertex vertex1 = submesh.Vertices[indices[i++]];
+                    SKNVertex vertex2 = submesh.Vertices[indices[i++]];
+                    SKNVertex vertex3 = submesh.Vertices[indices[i++]];
+
+                    PointF[] points = new PointF[]
+                    {
+                        new PointF(1024 * vertex1.UV.X, 1024 * vertex1.UV.Y),
+                        new PointF(1024 * vertex2.UV.X, 1024 * vertex2.UV.Y),
+                        new PointF(1024 * vertex3.UV.X, 1024 * vertex3.UV.Y),
+                        new PointF(1024 * vertex1.UV.X, 1024 * vertex1.UV.Y) //4th point to close the edge loop
+                    };
+
+                    DrawLines(image, points);
+                }
+
+                string submeshLocation = string.Format(@"{0}\{1}.png", exportDirectory, submesh.Name);
+                image.SaveAsPng(File.Create(submeshLocation));
+            }
+        }
+
+        private static Image CreateImage()
+        {
+            return new Image<Rgba32>(1024, 1024);
+        }
+        private static void DrawLines(Image image, PointF[] points)
+        {
+            image.Mutate(x => x.DrawLines(BRUSH, 0.75f, points));
+        }
+    }
+}
---
+++ Uvee/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Fantome.Libraries.League.Helpers;
+using System.IO;
+using Fantome.Libraries.League.IO.SimpleSkin;
+
+namespace Uvee
+{
+#warning Build: dotnet publish -c Release -r win-x86 --self-contained true /p:PublishSingleFile=true /p:TrimUnusedDependencies=true
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                if (File.Exists(args[0]))
+                {
+                    LeagueFileType meshType = Utilities.GetExtensionType(Path.GetExtension(args[0]));
+
+                    if(meshType == LeagueFileType.SKN)
+                    {
+                        Processor.Process(args[0], new SKNFile(args[0]));
+                    }
+                    else
+                    {
+                        WriteError("File type must be SKN");
+                    }
+
+                }
+                else
+                {
+                    WriteError("File does not exist");
+                }
+            }
+            else
+            {
+                WriteError("Please input SKN file");
+            }
+        }
+
+        private static void WriteError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
